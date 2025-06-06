3665e268d67c188fdf88a04d69b5664f67952d6e
Evi Velentza <paraskevivelentza@gmail.com>
2025-06-06 10:30:12 +0200

    Table class Only

--- utils/table.py
+++ utils/table.py
@@ -1,1 +1,10 @@
+class Table:
+    def __init(self, capacity=4):
+        self._capacity = capacity
+        self._seats = []
 
+        count = 0
+        while count < capacity:
+            seat = Seat()
+            self._seats.append(seat)
+            count = count + 1
