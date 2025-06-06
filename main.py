fdcd90ba8e21e69c6d68a22760df64be0c8fd801
Evi Velentza <paraskevivelentza@gmail.com>
2025-06-06 14:59:44 +0200

    step 3 updated main.py

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
+import csv
+from openspace import Openspace 
+
+def load_names_file (filename):
+    """ Read CSV file and send names to a list 
+    """
+    names = []
+    with open(filename, 'r') as file:
+        reader = csv.reader(file)
+        for row in reader:
+            name = row[0]
+            names.append(name)
+            
+    return names
+
+if __name__ == "__main__":
+    names = load_names_file("colleagues.csv")
+    openspace = Openspace()
+    openspace.organize(names)
+    openspace.display()
+
+
 
